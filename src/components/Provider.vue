d1c039a0c8151b2603a3922db075b8c05275509c
邓安佳 <2270588502@qq.com>
2022-12-06 21:16:33 +0800

    修复websocket长链接问题

--- src/components/Provider.vue
+++ src/components/Provider.vue
@@ -4,7 +4,7 @@
 <script setup lang="ts">
 // 绑定组件方法至window全局
 import { useMessage, useDialog, useNotification, useLoadingBar } from "naive-ui";
-import { provide } from "vue";
+import { onUnmounted, provide } from "vue";
 
 const { ipcRenderer } = require("electron");
 
@@ -18,12 +18,14 @@ window["$notification"] = notification;
 window["$loadingBar"] = loadingBar;
 
 // socket 连接
-let socketUrl = "ws://127.0.0.1";
-if (import.meta.env.PROD) {
-	socketUrl = "ws://www.chiyu.site/doit";
-}
-const socket = new WebSocket(socketUrl);
-socket.onmessage = e => {
+let socketUrl = import.meta.env.PROD ? "ws://www.chiyu.site/ws" : "ws://127.0.0.1";
+let socket: WebSocket;
+// 重新连接锁
+let lockReconnect = false;
+
+// 接收消息
+const websocketOnMessage = (e: MessageEvent) => {
+	heartbeat();
 	const data = JSON.parse(e.data);
 	if (data.event === "set-ws-key") {
 		localStorage.setItem("ws-key", data.data);
@@ -31,9 +33,53 @@ socket.onmessage = e => {
 		ipcRenderer.send("notification", data);
 	}
 };
+// 连接失败
+const websocketOnError = () => {
+	connectSocket();
+};
+//连接断开
+const websocketOnClose = () => {
+	if (lockReconnect) {
+		connectSocket();
+	}
+};
+let interval = undefined;
+let timeout = undefined;
+//心跳检测
+const heartbeat = () => {
+	clearInterval(interval);
+	clearTimeout(timeout);
+	interval = setInterval(() => {
+		let data = {
+			event: "events",
+			data: "ping",
+		};
+		socket.send(JSON.stringify(data));
+		timeout = setTimeout(() => {
+			lockReconnect = true;
+			socket.close(3982, localStorage.getItem("ws-key") ?? "");
+		}, 2000);
+	}, 30000);
+};
+//连接websocket
+const connectSocket = () => {
+	socket = new WebSocket(socketUrl);
+	socket.onerror = websocketOnError;
+	socket.onclose = websocketOnClose;
+	socket.onmessage = websocketOnMessage;
+};
+connectSocket();
 
 ipcRenderer.on("close", () => {
 	socket.close(3982, localStorage.getItem("ws-key") ?? "");
+});
+onUnmounted(() => {
+	lockReconnect = false;
+	if (socket) {
+		socket.close(3982, localStorage.getItem("ws-key") ?? "");
+	}
+	clearInterval(interval);
+	clearTimeout(timeout);
 });
 </script>
 
